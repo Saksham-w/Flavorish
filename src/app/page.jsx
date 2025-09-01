889876e502e86a746f22cea49b389f9d50371efb
saksham <sakshamshrestha400@gmail.com>
2025-09-01 19:23:43 +0545

    added responsive layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,5 +2,22 @@ import Link from "next/link";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <Link href="/">Hello</Link>;
+  return (
+    <Link href="/">
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia
+        recusandae magnam, laboriosam soluta nulla amet qui exercitationem
+        voluptate? Error sapiente inventore cumque, autem quae voluptatum
+        recusandae natus quo a dolores quaerat maxime voluptas praesentium
+        officia perferendis ratione veniam illum? Aperiam.
+      </h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sequi
+      itaque reprehenderit earum eius! Saepe similique eveniet rerum temporibus
+      possimus assumenda, reprehenderit nisi ipsam esse ut doloremque soluta
+      amet. Deserunt, eligendi recusandae! Veritatis optio corporis natus
+      nesciunt temporibus architecto pariatur recusandae consequuntur cumque
+      quam accusantium, modi, fuga maiores deleniti odit dignissimos excepturi,
+      impedit labore qui doloremque vitae perspiciatis a in.
+    </Link>
+  );
 }
