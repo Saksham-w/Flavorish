49d5e0cb98f1341e78c21056de3a289af0cd9bfc
Saksham-w <sakshamshrestha400@gmail.com>
2025-10-07 18:26:00 +0545

    feat: add page layout with all components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,19 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/menu/Menu";
 
 export default function Home() {
   return (
-    <Link href="/">
-      <h1>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia
-        recusandae magnam, laboriosam soluta nulla amet qui exercitationem
-        voluptate? Error sapiente inventore cumque, autem quae voluptatum
-        recusandae natus quo a dolores quaerat maxime voluptas praesentium
-        officia perferendis ratione veniam illum? Aperiam.
-      </h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sequi
-      itaque reprehenderit earum eius! Saepe similique eveniet rerum temporibus
-      possimus assumenda, reprehenderit nisi ipsam esse ut doloremque soluta
-      amet. Deserunt, eligendi recusandae! Veritatis optio corporis natus
-      nesciunt temporibus architecto pariatur recusandae consequuntur cumque
-      quam accusantium, modi, fuga maiores deleniti odit dignissimos excepturi,
-      impedit labore qui doloremque vitae perspiciatis a in.
-    </Link>
+    <div className={styles.container}>
+      <Featured/>
+      <CategoryList/>
+      <div className={styles.content}>
+        <CardList/>
+        <Menu/>
+      </div>
+    </div>
   );
 }
