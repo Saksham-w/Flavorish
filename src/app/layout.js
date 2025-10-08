b6a0508419e64fdbba1e42604fe9532bee7848f8
Saksham-w <sakshamshrestha400@gmail.com>
2025-10-08 16:57:07 +0545

    feat: add dark mode and responsive hamburger menu

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,9 @@ import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/footer/Footer";
+import { ThemeContext } from "@/context/ThemeContext";
+import { ThemeContextProvider } from "@/context/ThemeContext";
+import ThemeProvider from "@/providers/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,13 +17,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="container">
-          <div className="wrapper">
-            <Navbar />
-            {children}
-            <Footer />
-          </div>
-        </div>
+        <ThemeContextProvider>
+          <ThemeProvider>
+            <div className="container">
+              <div className="wrapper">
+                <Navbar />
+                {children}
+                <Footer />
+              </div>
+            </div>
+          </ThemeProvider>
+        </ThemeContextProvider>
       </body>
     </html>
   );
---
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -0,0 +1,33 @@
+"use client";
+import Image from "next/image";
+import React, { use, useContext } from "react";
+import styles from "./themeToggle.module.css";
+import { ThemeContext } from "@/context/ThemeContext";
+
+function ThemeToggle() {
+  const { theme, toggle } = useContext(ThemeContext);
+  console.log(theme);
+
+  return (
+    <div
+      className={styles.container}
+      onClick={toggle}
+      style={
+        theme === "dark" ? { backgroundColor: "white" } : { backgroundColor: "#0f172a" }
+      }
+    >
+      <Image src="/moon.png" alt="Moon Icon" width={14} height={14} />
+      <div
+        className={styles.ball}
+        style={
+          theme === "dark"
+            ? { left: "1px", background: "#0f172a" }
+            : { right: "1px", background: "white" }
+        }
+      ></div>
+      <Image src="/sun.png" alt="Sun Icon" width={14} height={14} />
+    </div>
+  );
+}
+
+export default ThemeToggle;
