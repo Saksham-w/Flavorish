f80d4805d85792ae81f1056a01e16aa38ce20504
Saksham-w <sakshamshrestha400@gmail.com>
2025-11-01 17:44:03 +0545

    feat: add pagination layout

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+"use client";
 
-function Pagination() {
+import React from "react";
+import styles from "./pagination.module.css";
+import { useRouter } from "next/navigation";
+
+const Pagination = ({ page, hasPrev, hasNext }) => {
+  const router = useRouter();
+
   return (
-    <div>Pagination</div>
-  )
-}
+    <div className={styles.container}>
+      <button className={styles.button}>Previous</button>
+      <button className={styles.button}>Next</button>
+    </div>
+  );
+};
 
-export default Pagination
+export default Pagination;
