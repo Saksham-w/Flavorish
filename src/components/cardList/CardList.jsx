a6d73a1fc499b9895f4e6b13859d2c303ab644fa
Saksham-w <sakshamshrestha400@gmail.com>
2025-11-01 17:40:17 +0545

    feat: add menu menucategories and menuposts design and layout

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import styles from './cardList.module.css'
-import Pagination from '../pagination/Pagination'
+import React from "react";
+import styles from "./cardList.module.css";
+import Pagination from "../pagination/Pagination";
+import Card from "../card/Card";
 
 function CardList() {
   return (
     <div className={styles.container}>
-        <Pagination />
+      <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <Pagination />
     </div>
-  )
+  );
 }
 
-export default CardList
+export default CardList;
