8a851b0352f036caa6c7b628175dc3862db1abb7
Saksham-w <sakshamshrestha400@gmail.com>
2025-11-04 17:33:43 +0545

    feat: add API endpoints for blog fetching acc to categories

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -4,17 +4,19 @@ import { NextResponse } from "next/server";
 export const GET = async (req) => {
   const { searchParams } = new URL(req.url);
   const page = parseInt(searchParams.get("page")) || 1;
+  const cat = searchParams.get("cat");
   const POST_PER_PAGE = 2;
 
-  const query = {
-    take: POST_PER_PAGE,
-    skip: POST_PER_PAGE * (page - 1),
-  };
+  const where = cat ? { catSlug: cat } : {};
 
   try {
     const [posts, count] = await prisma.$transaction([
-      prisma.post.findMany(query),
-      prisma.post.count(),
+      prisma.post.findMany({
+        take: POST_PER_PAGE,
+        skip: POST_PER_PAGE * (page - 1),
+        where,
+      }),
+      prisma.post.count({ where }),
     ]);
     return NextResponse.json({ posts, count }, { status: 200 });
   } catch (error) {
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -11,8 +11,10 @@ export default function Card({ key, item }) {
       </div>
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}>11.02.2023 - </span>
-          <span className={styles.category}>Culture</span>
+          <span className={styles.date}>
+            {item.createdAt.substring(0, 10)} -{" "}
+          </span>
+          <span className={styles.category}>{item.catSlug}</span>
         </div>
         <Link href="/">
           <h1>{item.title}</h1>
