bb085a05c1fef44e90f5b758ccadc1ce2ab47220
Saksham-w <sakshamshrestha400@gmail.com>
2025-11-10 19:56:29 +0545

    update: herosection update with new image and contents

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,25 +1,23 @@
-import React from 'react'
-import styles from './featured.module.css'
-import Image from 'next/image';
+import React from "react";
+import styles from "./featured.module.css";
+import Link from "next/link";
 
 function Featured() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>
-        <b>Hey, Saksham here!</b> Discover my stories and creative ideas
-      </h1>
-      <div className={styles.post}>
-        <div className={styles.imgContainer}>
-          <Image src= "/p1.jpeg" alt="Post Image" fill></Image>
-        </div>
-        <div className={styles.textContainer}>
-          <h2 className={styles.postTitle}>My First Post</h2>
-          <p className={styles.postDesc}>This is a description of my first post.</p>
-          <button className={styles.button}>Read More</button>
+      <div className={styles.overlay}>
+        <h1 className={styles.title}>Discover the Best Flavors of Nepal 🍲</h1>
+        <div className={styles.ctaButtons}>
+          <Link href="/blog" className={styles.primaryButton}>
+            Explore Blogs
+          </Link>
+          <Link href="/popular" className={styles.secondaryButton}>
+            Most Famous
+          </Link>
         </div>
       </div>
     </div>
   );
-};
+}
 
-export default Featured
+export default Featured;
